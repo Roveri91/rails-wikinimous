7248ce53d24c2ebee2b1646e8082ba5180d9a145
Simone Roveri <spikeroveri91@gmail.com>
2023-07-01 16:43:51 +0900

    create 10 random article and create index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Destroying all Article..."
+Article.destroy_all
+puts "DONE!"
+
+puts "Creating 10 fake article..."
+10.times do
+  article = Article.create!(
+    title: Faker::TvShows::DumbAndDumber.quote,  
+    content: Faker::String.random(length: 30..100)
+  )
+  puts "#{article.title} created."
+end
+
+puts "DONE!!"
